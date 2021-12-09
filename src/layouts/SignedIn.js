b7655362f0231a897fef1e30a25a8536d1e3c125
edaozcan <73548267+edaozcan@users.noreply.github.com>
2021-12-09 18:03:20 +0300

    ..

---
+++ src/layouts/SignedIn.js
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import { Dropdown, Menu,Image } from 'semantic-ui-react'
+
+export default function SignedIn({signOut}) {
+    return (
+        <div>
+            <Menu.Item>
+                <Image avatar spaced="right" src="https://avatars.mds.yandex.net/i?id=f30da3f72f4a005842cf76df0c7c7a56-4119103-images-thumbs&n=13"/>
+                <Dropdown pointing="top left" text="Eda">
+                    <Dropdown.Menu>
+                        <Dropdown.Item text="Bilgilerim" icon="info"/>
+                        <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out"/>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu.Item>
+        </div>
+    )
+}
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react'
+import { Link } from 'react-router-dom'
 import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 import ProductService from '../services/productService'
 
@@ -10,7 +11,7 @@ export default function ProductList() {
         let productService=new ProductService()
         productService.getProducts().then(result=>setProducts(result.data.data))
 
-    })
+    },[])
     return (
         <div>
  <Table celled>
@@ -28,7 +29,7 @@ export default function ProductList() {
         {
             products.map(product=>(
                 <Table.Row key={product.id}>
-        <Table.Cell>{product.productName}</Table.Cell>
+        <Table.Cell><Link to={`/products/ ${product.productName}`}>{product.productName}</Link></Table.Cell>
         <Table.Cell>{product.unitPrice}</Table.Cell>
         <Table.Cell>{product.unitsnInStock}</Table.Cell>
         <Table.Cell>{product.quantityPerUnit}</Table.Cell>
