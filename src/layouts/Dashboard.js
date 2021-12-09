00561e6f57be5b04b00e8e14a3b3b517c8c0c75a
edaozcan <73548267+edaozcan@users.noreply.github.com>
2021-12-10 02:23:33 +0300

    ..

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -20,8 +20,8 @@ export default function Dashboard() {
             
             <Route exact path="/" component={ProductList} />
             <Route exact path="/products" component={ProductList} />
-            <Route  path="/products/:name" component={ProductDetail} />
-            <Route  path="/cart" component={CartDetail} />
+            <Route path="/products/:name" component={ProductDetail} />
+            <Route path="/cart" component={CartDetail} />
             
             </Grid.Column>
             </Grid.Row>
